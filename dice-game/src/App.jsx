d9bd689e49343e4ac20fd21c49b1145854a5fb9a
omkar <omkarsbhatkande@gmail.com>
2024-09-16 09:35:30 +0530

    dice game setUp done images added

---
+++ dice-game/src/App.jsx
@@ -0,0 +1,19 @@
+import StartGame from "./components/StartGame"
+
+
+
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+  <StartGame />
+    </>
+  )
+}
+
+export default App
---
+++ dice-game/src/components/StartGame.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StartGame = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StartGame
