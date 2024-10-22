e04eff4d43934cb1839e5f5649596c69fca396b0
omkar <omkarsbhatkande@gmail.com>
2024-10-22 23:41:39 +0530

    issue solved

--- netflix-app/src/components/Lists.jsx
+++ netflix-app/src/components/Lists.jsx
@@ -1,30 +1,21 @@
+const Lists = ({ currElem }) => {
+  const { img_url, name, rating, description, genre, cast, watch_url } = currElem;
 
-
-const Lists = (currElem) => {
-  const {name,rating,description,genre,cast,watch_url} = currElem;
-  console.log(currElem);
-  
   return (
-    <div>
-        
-        <li >
-       <div>
-        {/* <img src=alt="" width="40%" height="40%" /> */}
+    <li>
+      <div>
+        <img src={img_url} alt={name} width="40%" height="40%" />
       </div>
-      <h2>Name   :  {name}</h2>
-      <h3>Rating : {rating} </h3>
-      <p>Summary : {description}  </p>
-      <p>genre   : {genre}   </p>
-      <p>Cast    : {cast}   </p>
-      <a href={watch_url}  target="_blank">
-      <button>Watch Now</button>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>Summary: {description}</p>
+      <p>Genre: {genre}</p>
+      <p>Cast: {cast}</p>
+      <a href={watch_url} target="_blank" rel="noopener noreferrer">
+        <button>Watch Now</button>
       </a>
-      
-
-      </li>
-        
-    </div>
-  )
+    </li>
+  );
 }
 
-export default Lists
+export default Lists;
--- netflix-app/src/components/NetfliexSeries.jsx
+++ netflix-app/src/components/NetfliexSeries.jsx
@@ -1,16 +1,15 @@
-import series from "../api/series.json"
-import Lists from "./Lists"
+import series from "../api/series.json";
+import Lists from "./Lists";
 
 const NetfliexSeries = () => {
   return (
-    <>
     <ul className="grid grid-three-cols">
-      {series.map((currElem)=>{ 
-        return  <Lists key={currElem.id} currElem={currElem} />
-         })}
-      </ul>
-    </>
-  )
+      {series.map((currElem) => { 
+        return <Lists key={currElem.id} currElem={currElem} />;
+      })}
+    </ul>
+  );
 }
 
 export default NetfliexSeries;
+
